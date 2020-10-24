fb05888a6404bbcba81f31c19a6b2c6aded7de2c
Eduardo <archivo.ehv@gmail.com>
2020-10-24 14:57:47 -0300

    Fourth commit - Entity

--- app/src/main/java/com/example/simulacion_plaplix/model/local/PhonesEntity.kt
+++ app/src/main/java/com/example/simulacion_plaplix/model/local/PhonesEntity.kt
@@ -1,4 +1,14 @@
 package com.example.simulacion_plaplix.model.local
 
-class PhonesEntity {
+import android.media.Image
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Phones_Table")
+data class PhonesEntity (@PrimaryKey @NonNull  val id: Int,
+                         val image: String,
+                         val name: String,
+                         val price: Int
+)
 }
